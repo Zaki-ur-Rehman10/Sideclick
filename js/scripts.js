ca90c850760e2efd0d078387cc3ff3277871d402
Zaki-ur-Rehman10 <reh53920@gmail.com>
2024-04-23 06:09:02 +0500

    html

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+var $ = jQuery;
+$(document).ready(function(){
+    $('#toggle-cat-dog').find('input[type="radio"]').change(function(){
+        var $this = $(this);
+        if($this.val() == 'cat'){
+            $('#options-for-cat-search').show();
+            $('#options-for-dog-search').hide();
+        }else if($this.val() == 'dog'){
+            $('#options-for-dog-search').show();
+            $('#options-for-cat-search').hide();
+        }else{
+            $('#options-for-dog-search').show();
+            $('#options-for-cat-search').hide();
+        }
+        return false;
+    });
+    $('#trigger-menu-sidebar').on('click' , function(){
+        $('#the-main-menu').slideToggle()
+        $(this).toggleClass('active');
+        return false;
+    })
+    $('body').on('click', ".dash-menu-item" , function(e){
+        e.preventDefault();
+        var id 	=	$(this).data('tab');
+        // 
+        $('.tab[data-tab='+id+']').addClass('active').siblings().removeClass('active');
+        $(this).addClass('active').siblings().removeClass('active');
+
+    })
+})
