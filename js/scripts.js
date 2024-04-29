8c1bc156b83011c5ee4cd66bcb9b1cd7f2809b5b
Zaki-ur-Rehman10 <reh53920@gmail.com>
2024-04-29 09:34:20 +0500

    html

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 var $ = jQuery;
 $(document).ready(function(){
    
-    $('.js-example-basic-single').select2();
-
+    $('body').on('click', "a.side-bar-open" , function(e){
+        e.preventDefault();
+        $('.dashboard-sidebar').toggleClass('active');
+        $('.dashboard-inner > .row > .col-md-3').toggleClass('active');
+    })
+    $('body').on('click', "a.messages-side-bar-open" , function(e){
+        e.preventDefault();
+        $('.messages-section .col-md-4').toggleClass('active');
+        $('.messages-section .users-message-list').toggleClass('active');
+    })
+    
     $('#toggle-cat-dog').find('input[type="radio"]').change(function(){
         var $this = $(this);
         if($this.val() == 'cat'){
@@ -22,14 +31,13 @@ $(document).ready(function(){
         $(this).toggleClass('active');
         return false;
     });
-    $('body').on('click', ".dash-menu-item" , function(e){
-        e.preventDefault();
-        var id 	=	$(this).data('tab');
-        // 
-        $('.tab[data-tab='+id+']').addClass('active').siblings().removeClass('active');
-        $(this).addClass('active').siblings().removeClass('active');
+    // $('body').on('click', ".dash-menu-item" , function(e){
+    //     e.preventDefault();
+    //     var id 	=	$(this).data('tab');
+    //     $('.tab[data-tab='+id+']').addClass('active').siblings().removeClass('active');
+    //     $(this).addClass('active').siblings().removeClass('active');
 
-    });
+    // });
     $('body').on('click', ".list-filter" , function(e){
         e.preventDefault();
         $('.sitter-tabs').addClass('active');
@@ -42,10 +50,9 @@ $(document).ready(function(){
         $(this).addClass('active').siblings().removeClass('active');
 
     });
-    $('body').on('click', ".sitter-profile-btns > a" , function(){
-        $(this).addClass('active').siblings().removeClass('active');
-
-    })
+    
+    $('.js-example-basic-single').select2();
+    
     
     
 })
