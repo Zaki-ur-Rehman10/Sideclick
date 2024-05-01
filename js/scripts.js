ac5ab91fab60dab6815606b103776e6305d80946
Zaki-ur-Rehman10 <reh53920@gmail.com>
2024-05-01 18:45:08 +0500

    html

--- js/scripts.js
+++ js/scripts.js
@@ -53,6 +53,143 @@ $(document).ready(function(){
     
     $('.js-example-basic-single').select2();
     
-    
+    $(function() {
+          $("#calendar").datepicker({
+            onSelect: function(dateText, inst) {
+              var selectedDate = new Date(dateText);
+              var dateString = $.datepicker.formatDate('yy-mm-dd', selectedDate);
+              var index = selectedDates.indexOf(dateString);
+              if (index === -1) {
+                selectedDates.push(dateString);
+                $(this).find('td.ui-datepicker-current-day').addClass('selected');
+              } else {
+                selectedDates.splice(index, 1);
+                $(this).find('td.ui-datepicker-current-day').removeClass('selected');
+              }
+              updateSelectedCount();
+            },
+            beforeShowDay: function(date) {
+              var dateString = $.datepicker.formatDate('yy-mm-dd', date);
+              if ($.inArray(dateString, selectedDates) != -1) {
+                return [true, "selected"];
+              }
+              return [true, ""];
+            }
+          });
+      
+          var selectedDates = [];
+      
+          $(document).on('dblclick', '#calendar .ui-datepicker-calendar tbody td', function() {
+            var date = $(this).text();
+            var month = $("#calendar .ui-datepicker-month").val();
+            var year = $("#calendar .ui-datepicker-year").val();
+            var dateString = year + '-' + (parseInt(month) + 1) + '-' + date;
+            if (!$(this).hasClass('ui-datepicker-unselectable')) {
+              var index = selectedDates.indexOf(dateString);
+              if (index === -1) {
+                selectedDates.push(dateString);
+                $(this).addClass('selected');
+              } else {
+                selectedDates.splice(index, 1);
+                $(this).removeClass('selected');
+              }
+              updateSelectedCount();
+            }
+          });
+      
+          $(document).on('click', '#clearButton', function() {
+            $("#calendar .selected").removeClass('selected');
+            selectedDates = [];
+            updateSelectedCount();
+          });
+      
+          function updateSelectedCount() {
+            var count = selectedDates.length;
+            $("#selectedCount").text(count + " Day selected");
+          }
+        });
     
 })
+
+
+$(function() {
+    $("#calendar").datepicker({
+      onSelect: function(dateText, inst) {
+        var selectedDate = new Date(dateText);
+        var dateString = $.datepicker.formatDate('yy-mm-dd', selectedDate);
+        var index = selectedDates.indexOf(dateString);
+        if (index === -1) {
+          selectedDates.push(dateString);
+          $(this).find('td.ui-datepicker-current-day').addClass('selected');
+        } else {
+          selectedDates.splice(index, 1);
+          $(this).find('td.ui-datepicker-current-day').removeClass('selected');
+        }
+        updateSelectedCount();
+      },
+      beforeShowDay: function(date) {
+        var dateString = $.datepicker.formatDate('yy-mm-dd', date);
+        if ($.inArray(dateString, selectedDates) != -1) {
+          return [true, "selected"];
+        }
+        return [true, ""];
+      }
+    });
+
+    var selectedDates = [];
+
+    $(document).on('dblclick', '#calendar .ui-datepicker-calendar tbody td', function() {
+      var date = $(this).text();
+      var month = $("#calendar .ui-datepicker-month").val();
+      var year = $("#calendar .ui-datepicker-year").val();
+      var dateString = year + '-' + (parseInt(month) + 1) + '-' + date;
+      if (!$(this).hasClass('ui-datepicker-unselectable')) {
+        var index = selectedDates.indexOf(dateString);
+        if (index === -1) {
+          selectedDates.push(dateString);
+          $(this).addClass('selected');
+        } else {
+          selectedDates.splice(index, 1);
+          $(this).removeClass('selected');
+        }
+        updateSelectedCount();
+      }
+    });
+
+    $(document).on('click', '#clearButton', function() {
+      $("#calendar .selected").removeClass('selected');
+      selectedDates = [];
+      updateSelectedCount();
+    });
+
+    function updateSelectedCount() {
+      var count = selectedDates.length;
+      $("#selectedCount").text(count + " Day selected");
+    }
+  });
+  
+
+  var slider3 = new rSlider({
+    target: '#sampleSlider',
+    values: {min: 0, max: 50},
+    step: 5,
+    range: true,
+    set: [0, 50],
+    scale: false,
+    labels: false,
+    onChange: function (vals) {
+        console.log(vals);
+    }
+});
+var slider2 = new rSlider({
+    target: '#sampleSlider2',
+    values: {min: 2, max: 19},
+    step: 5,
+    range: true,
+    set: [0, 50],
+    scale: false,
+    labels: false,
+    onChange: function (vals) {
+        console.log(vals);
+    }
+});
